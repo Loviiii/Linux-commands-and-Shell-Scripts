a939c3b45078dc3d445e431d81a5989e9b243628
Sahil <149608775+Loviiii@users.noreply.github.com>
2026-06-18 13:30:10 +0530

    Update backup.sh

--- Linux Commands and Shell scripts By IBM (Coursera)/Finel Project/Project Submission Uploaded Files/backup.sh
+++ Linux Commands and Shell scripts By IBM (Coursera)/Finel Project/Project Submission Uploaded Files/backup.sh
@@ -1,69 +1,60 @@
 #!/bin/bash
 
-# This checks if the number of arguments is correct
-# If the number of arguments is incorrect ( $# != 2) print error message and exit
 if [[ $# != 2 ]]
 then
   echo "backup.sh target_directory_name destination_directory_name"
   exit
 fi
 
-# This checks if argument 1 and argument 2 are valid directory paths
 if [[ ! -d $1 ]] || [[ ! -d $2 ]]
 then
   echo "Invalid directory path provided"
   exit
 fi
 
-# [TASK 1]
+# Task 1
 targetDirectory=$1
 destinationDirectory=$2
 
-# [TASK 2]
-echo "targetDirectory is $targetDirectory"
-echo "destinationDirectory is $destinationDirectory"
+# Task 2
+echo $targetDirectory
+echo $destinationDirectory
 
-# [TASK 3]
-currentTS=$(date +%s)
-backupFileName="backup-$currentTS.tar.gz"
+# Task 3
+currentTS=$(date '+%s')
+backupFileName="backup-${currentTS}.tar.gz"
 
-# [TASK 5]
+# Task 5
 origAbsPath=$(pwd)
 
-# [TASK 6]
-cd "$targetDirectory"
-targetAbsPath=$(pwd)
-
-# go back
-cd "$origAbsPath"
-
-# [TASK 7]
+# Task 6
 cd "$destinationDirectory"
 destAbsPath=$(pwd)
 
-# go to target again
-cd "$targetAbsPath"
+# Task 7
+cd "$origAbsPath"
+cd "$targetDirectory"
 
-# [TASK 8]
-yesterdayTS=$(($(date +%s) - 86400))
+# Task 8
+yesterdayTS=$(($currentTS - 24 * 60 * 60))
 
 declare -a toBackup
 
-# [TASK 9]
+# Task 9
 for file in *
 do
-  # [TASK 10]
-  if [ -f "$file" ]
+  # Task 10
+  fileTS=$(date -r "$file" +%s)
+
+  if [[ $fileTS -ge $yesterdayTS ]]
   then
-    # [TASK 11]
+    # Task 11
     toBackup+=("$file")
   fi
 done
 
-# [TASK 12]
+# Task 12
 tar -czvf "$backupFileName" "${toBackup[@]}"
 
-# [TASK 13] (IMPORTANT FIX)
-mv "$backupFileName" "$destAbsPath/backup-$currentTS.tar.gz"
-
-# Congratulations! You completed the final project for this course!
+# Task 13
+mv "$backupFileName" "$destAbsPath"
